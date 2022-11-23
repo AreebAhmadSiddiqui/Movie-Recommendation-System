be927db54428ff593e182608ca8f9c23a5cf116b
Areeb Ahmad Siddiqui <87814983+AreebAhmadSiddiqui@users.noreply.github.com>
2022-11-23 13:51:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def recommender(new_movies, movie_name):
     for i in movies_list:
         recommended_movies.append(new_movies.iloc[i[0]].title)
         response_API = requests.get(
-            "https://api.themoviedb.org/3/movie/{}?api_key=8f655507141a5d524fc2024c9f76b6c7&language=en-US".format(new_movies.iloc[i[0]].id)).json()
+            "https://api.themoviedb.org/3/movie/{}?api_key={YOUR_API_KEY}&language=en-US".format(new_movies.iloc[i[0]].id)).json()
         poster_paths.append(
             "https://image.tmdb.org/t/p/original"+response_API['poster_path'])
 
